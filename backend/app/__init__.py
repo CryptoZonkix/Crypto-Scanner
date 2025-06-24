ee308272aeaf8491892bdca3a24c8546b271537c
CryptoZonkix <138564433+CryptoZonkix@users.noreply.github.com>
2025-06-24 12:01:14 +0100

    Add files via upload

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -8,7 +9,7 @@ socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'supersecret')
     CORS(app)
     db.init_app(app)
